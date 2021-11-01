4bdb6fd06495c5c5cae353bbdddadbb4f95878c9
atyu1 <attila.sovak@cisco.com>
2021-11-01 16:12:39 +0100

    Fix sensor self and super

--- src/sensor/dht11.py
+++ src/sensor/dht11.py
@@ -11,12 +11,13 @@ _PIN = board.D23
 class Dht11Sensor(Sensor):
     """ DHT11 Sensor """
     def __init__(self):
+        super().__init__(self)
         self.sensor = adafruit_dht.DHT11(_PIN)
     
     def run(self):
         try:
-            temperature = {"temperature": sensor.temperature}
-            humidity = {"humidity": sensor.humidity}
+            temperature = {"temperature": self.sensor.temperature}
+            humidity = {"humidity": self.sensor.humidity}
             return {"data":[temperature, humidity]}
         except RuntimeError as error:
             raise SensorError(f"Wrong data from DHT11 sensor, {error.args[0]}")
