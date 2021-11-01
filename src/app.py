2b98e147bbf403a61dcaa6cf7d36cf003b1069eb
atyu1 <attila.sovak@cisco.com>
2021-11-01 15:53:26 +0100

    remove dotenv due to bug

--- src/app.py
+++ src/app.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 
 from sensor.dht11 import Dht11Sensor as Sensor
-from dotenv import load_dotenv
 import os
 import logger
 
 def main():
-
-    load_dotenv()
 
     dht = Sensor()
     data = dht.run()
