25447f3d5c60dd38948673b00b81000f6c53bd20
atyu1 <attila.sovak@cisco.com>
2021-11-01 14:29:15 +0100

    dht11 sensor implementation

---
+++ src/sensor/dht11.py
@@ -0,0 +1,24 @@
+from sensor.sensor import Sensor
+from sensor.sensor import SensorError
+
+import time
+import board
+import adafruit_dht
+from src import sensor
+
+_PIN = board.D23
+
+
+class Dht11Sensor(Sensor):
+    """ DHT11 Sensor """
+    def __init__(self):
+        self.sensor = adafruit_dht.DHT11(_PIN)
+    
+    def run(self):
+        data = 
+        try:
+            temperature = {"temperature": sensor.temperature}
+            humidity = {"humidity": sensor.humidity}
+            return {"data":[temperature, humidity]}
+        except RuntimeError as error:
+            raise SensorError(f"Wrong data from DHT11 sensor, {error.args[0]}")
