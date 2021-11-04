5a4ba4d2ab237f6d6a5793bff978122972f93dfb
atyu1 <attila.sovak@cisco.com>
2021-11-04 13:55:03 +0100

    catch handler errors

--- src/app.py
+++ src/app.py
@@ -6,17 +6,20 @@ import os
 import logging
 
 from sensor.sensor import SensorError
+from src.handler.handler import HandlerError
 
 
 def main():
 
     dht = Sensor()
     data = {}
-    
+
     try:
         data = dht.run()
     except SensorError as e:
         logging.error(f"Issue with DHT Sensor: {e}")
+    except HandlerError as e:
+        logging.error(f"Issue with Handler: {e}")
 
     gpa = GPA(raw_data=data)
     gpa.save()
